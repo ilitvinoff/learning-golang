8ba7cf4d3ee111cc2fb3a2fe787f2c2b2842d088
ilitvinoff <69355499+ilitvinoff@users.noreply.github.com>
2020-10-31 12:42:39 +0200

    Add files via upload

--- web/server/main.go
+++ web/server/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	iconPath = "server/favicon_io.zip"
 )
 
 //Counter counts amount of visits to web server
@@ -14,21 +22,98 @@ type Counter struct {
 	value int
 }
 
+var db *sql.DB
+
 func main() {
-	// Hello world, the web server
+	user := printDataIn("enter username, please: ")
+	password := printDataIn("enter the password, please: ")
+	dbOpenStr := fmt.Sprint(user, ":", password, "@/counterDB")
+	//dbOpenStr := fmt.Sprint("dbuser:ololo@/counterDB")
+
+	db, err := sql.Open("mysql", dbOpenStr)
+	if err != nil {
+		log.Fatal("Can't open database:\n" + err.Error())
+	}
+	defer db.Close()
+
 	c := &Counter{Mut: &sync.Mutex{}, value: 0}
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, counter(c))
+	countHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, counter(c, req, db))
 	}
 
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	showBaseHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, showBase(db))
+	}
+
+	http.HandleFunc("/favicon.ico", favIconHandler)
+	http.HandleFunc("/", countHandler)
+	http.HandleFunc("/showbase", showBaseHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-func counter(c *Counter) string {
+func favIconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, iconPath)
+	fmt.Println("Icon")
+}
+
+func counter(c *Counter, req *http.Request, db *sql.DB) string {
 	c.Mut.Lock()
 	defer c.Mut.Unlock()
 	c.value++
-	return strconv.Itoa(c.value)
+	counterAsString := strconv.Itoa(c.value)
+
+	if addressExits(req.RemoteAddr, db) {
+		query := fmt.Sprintf("update counterDB.info set visits = %v where remoteAddr = '%v'", counterAsString, req.RemoteAddr)
+		result, err := db.Exec(query)
+		if err != nil {
+			s := fmt.Sprint("result: ", result, "; error: ", err.Error())
+			return s
+		}
+		return counterAsString
+	}
+
+	result, err := db.Exec("insert into counterDB.info (remoteAddr,visits) values(?, ?)", req.RemoteAddr, counterAsString)
+	if err != nil {
+		s := fmt.Sprint("result: ", result, "; error: ", err.Error())
+		return s
+	}
+	return counterAsString
+}
+
+func showBase(db *sql.DB) string {
+	rows, err := db.Query("select * from counterDB.info")
+	if err != nil {
+		return "can't show database: " + err.Error()
+	}
+
+	result := ""
+
+	for rows.Next() {
+		buffer := struct {
+			remoteAddr string
+			visits     string
+		}{}
+		err = rows.Scan(&buffer.remoteAddr, &buffer.visits)
+		if err != nil {
+			return err.Error()
+		}
+		result = fmt.Sprint(result, "remote address: ", buffer.remoteAddr, "; visits: ", buffer.visits, "\n")
+	}
+
+	return result
+}
+
+func addressExits(addr string, db *sql.DB) bool {
+	var indicator int
+	query := fmt.Sprintf("select exists(select 1 from counterDB.info where remoteAddr = '%v')", addr)
+	row := db.QueryRow(query)
+
+	err := row.Scan(&indicator)
+	if err != nil {
+		fmt.Println("err: " + err.Error())
+		return false
+	}
+
+	return indicator == 1
 }
