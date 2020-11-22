d7beecc6774291bc3e9c3e6ebe952ef48e6768e9
ilitvinoff <69355499+ilitvinoff@users.noreply.github.com>
2020-11-22 20:47:30 +0200

    Add files via upload

--- tail/flags.go
+++ tail/flags.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -18,10 +19,6 @@ type regexFlag []*config
 type configFlag []*config
 
 func (p *pathFlag) Set(value string) error {
-	if value == "" {
-		return fmt.Errorf("empty path set with flag '-p'")
-	}
-
 	args := strings.Split(value, ";")
 	argsLength := len(args)
 
@@ -58,10 +55,6 @@ func (p *pathFlag) String() string {
 }
 
 func (r *regexFlag) Set(value string) error {
-	if value == "" {
-		return fmt.Errorf("empty path set with flag '-r'")
-	}
-
 	args := strings.Split(value, ";")
 	argsLength := len(args)
 
@@ -102,10 +95,6 @@ func (r *regexFlag) String() string {
 }
 
 func (c *configFlag) Set(value string) error {
-	if value == "" {
-		return fmt.Errorf("empty path set with flag '-c'")
-	}
-
 	args := strings.Split(value, ";")
 	argsLength := len(args)
 
@@ -149,6 +138,14 @@ func getConfigFromFlags() []*config {
 	var regexFlag regexFlag
 	var configFlag configFlag
 	var n int
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: tail -flag \"configuration string\" -flag1 \"configuration string\"...\n	Print the last 10 lines of each FILE to standard output.\n\nThe basic functionality is the same as the standard tail utility started with '-F' flag.\n\nAdded functionality:\n	1. You can \"tail\" multiple files at the same time. This way, you can define a \"prefix\" for each file, \n	which will be printed before the line from the corresponding file.\n	2. If file - doesn't exist -> wait for it to appear\n	3. If the file has been deleted / moved -> wait for a new one to appear. When new one appeared, tailing starts from the begining of the file.\n	4. You may select directory and define the regular expression(for file name).\n	This way, 'tailer' will keep track of the last file that came up that matches the regular expression.\n\nWarning!!!!\nYou need to describe parameters of each flag with 1 stringline. So, for example you need to tail 2 files, then both files\nyou'll describe in one string, using semicolon, as delimiter.\n\n	Example:\n	./tail -p \"filepath1;prefix for output from file1;filepath2;prefix for output from file2...\"\n\nWarning!!!\nDo not use semicolon at the end of argument line. %s:\n", os.Args[0])
+
+		flag.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(os.Stderr, "	-%v\n%v\n", f.Name, f.Usage) // f.Name, f.Value
+		})
+	}
 
 	flag.Var(&pathFlag, "p", "File's path to tail.If you want to specify 1 file path, 1 argument is enough - the file path.\nYou may specify prefix to printout it before textline from file, using semicolon as delimiter. Example: tail -p \"foo/bar/file;prefix\"\nIf you want to specify more then 1 file, you need to define both parameters(file's path;prefix) for each file in one string.\nExample: tail -p \"foo/bar/file1;prefix1;foo/bar/file2;prefix2;foo/bar/file3;prefix3...\"\nWarning!!!! Do not use semicolon at the end of argument line.\n")
 	flag.Var(&regexFlag, "r", "Filename regular expression pattern. If you want to specify 1 file to tail, 2 arguments are enough - \"pathToDirectory;regex\".\nIf you want to specify more then 1 file, you need to define 3 arguments for each file: \nExample: tail -r \"foo/bar/file1;regexForFile1;prefix1;foo/bar/file2;regexForFile2;prefix2;foo/bar/file3;regexForFile3;prefix3...\"\nWarning!!!! Do not use semicolon at the end of argument line.\n ")
