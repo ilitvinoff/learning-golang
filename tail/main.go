a3fab59cdabc7435959b3fce756f4251b40be15b
ilitvinoff <69355499+ilitvinoff@users.noreply.github.com>
2021-02-11 18:24:24 +0200

    Update main.go

--- tail/main.go
+++ tail/main.go
@@ -11,7 +11,6 @@ func main() {
 	if len(tailState.configArr) < 1 {
 		fmt.Println("Use flags to tweak the config you want. See help message. Enter: \n./tail -h")
 	}
-	fmt.Println("isDebug:", isDebug)
 	ConcurentTail(tailState)
 
 }
